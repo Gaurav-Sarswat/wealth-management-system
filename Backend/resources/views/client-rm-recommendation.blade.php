82126f5de2385a4c960646a99a3927be5226489f
somya-anchalia <sanchlia96@gmail.com>
2023-03-09 01:39:09 +0000

    cleaning rm recomendation page

--- Backend/resources/views/client-rm-recommendation.blade.php
+++ Backend/resources/views/client-rm-recommendation.blade.php
@@ -29,80 +29,75 @@
             </div>
             <!-- Start Dynamic Sections Starts here -->
             <section class="investment-lists pt-4 pb-5">
-                          
-        
-                            <div class="container">
-                                
-                                <div class="row mt-3">
-                                    <div class="col-4 mb-3">
-                                        <div class="custom-card">
-                                            <figure>
-                                                <img src="/images/common/mainfarm.png" class="w-100" alt="">
-                                            </figure>
-                                            <p class="mb-2 title">Agro Farming Project</p>
-                                            <p class="mb-2 content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati facere veritatis similique ipsum illo dignissimos.</p>
-                                            <span class="d-block mb-2 date">15-03-2023</span>
-                                            <a href="FIXME:" class="btn btn-custom px-4 py-2 d-flex align-items-center justify-content-center">Read More <i class="fas fa-arrow-right ml-2"></i></a>
-                                        </div>
-                                    </div>
-                                    <div class="col-4 mb-3">
-                                        <div class="custom-card">
-                                            <figure>
-                                                <img src="/images/common/mainfarm.png" class="w-100" alt="">
-                                            </figure>
-                                            <p class="mb-2 title">Agro Farming Project</p>
-                                            <p class="mb-2 content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati facere veritatis similique ipsum illo dignissimos.</p>
-                                            <span class="d-block mb-2 date">15-03-2023</span>
-                                            <a href="FIXME:" class="btn btn-custom px-4 py-2 d-flex align-items-center justify-content-center">Read More <i class="fas fa-arrow-right ml-2"></i></a>
-                                        </div>
-                                    </div><div class="col-4 mb-3">
-                                        <div class="custom-card">
-                                            <figure>
-                                                <img src="/images/common/mainfarm.png" class="w-100" alt="">
-                                            </figure>
-                                            <p class="mb-2 title">Agro Farming Project</p>
-                                            <p class="mb-2 content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati facere veritatis similique ipsum illo dignissimos.</p>
-                                            <span class="d-block mb-2 date">15-03-2023</span>
-                                            <a href="FIXME:" class="btn btn-custom px-4 py-2 d-flex align-items-center justify-content-center">Read More <i class="fas fa-arrow-right ml-2"></i></a>
-                                        </div>
-                                    </div>
-                                    <div class="col-4 mb-3">
-                                        <div class="custom-card">
-                                            <figure>
-                                                <img src="/images/common/mainfarm.png" class="w-100" alt="">
-                                            </figure>
-                                            <p class="mb-2 title">Agro Farming Project</p>
-                                            <p class="mb-2 content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati facere veritatis similique ipsum illo dignissimos.</p>
-                                            <span class="d-block mb-2 date">15-03-2023</span>
-                                            <a href="FIXME:" class="btn btn-custom px-4 py-2 d-flex align-items-center justify-content-center">Read More <i class="fas fa-arrow-right ml-2"></i></a>
-                                        </div>
-                                    </div>
-                                    <div class="col-4 mb-3">
-                                        <div class="custom-card">
-                                            <figure>
-                                                <img src="/images/common/mainfarm.png" class="w-100" alt="">
-                                            </figure>
-                                            <p class="mb-2 title">Agro Farming Project</p>
-                                            <p class="mb-2 content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati facere veritatis similique ipsum illo dignissimos.</p>
-                                            <span class="d-block mb-2 date">15-03-2023</span>
-                                            <a href="FIXME:" class="btn btn-custom px-4 py-2 d-flex align-items-center justify-content-center">Read More <i class="fas fa-arrow-right ml-2"></i></a>
-                                        </div>
-                                    </div><div class="col-4 mb-3">
-                                        <div class="custom-card">
-                                            <figure>
-                                                <img src="/images/common/mainfarm.png" class="w-100" alt="">
-                                            </figure>
-                                            <p class="mb-2 title">Agro Farming Project</p>
-                                            <p class="mb-2 content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati facere veritatis similique ipsum illo dignissimos.</p>
-                                            <span class="d-block mb-2 date">15-03-2023</span>
-                                            <a href="FIXME:" class="btn btn-custom px-4 py-2 d-flex align-items-center justify-content-center">Read More <i class="fas fa-arrow-right ml-2"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
+                <div class="container">
+                    <div class="row mt-3">
+                        <div class="col-4 mb-3">
+                            <div class="custom-card">
+                                <figure>
+                                    <img src="/images/common/mainfarm.png" class="w-100" alt="">
+                                </figure>
+                                <p class="mb-2 title">Agro Farming Project</p>
+                                <p class="mb-2 content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati facere veritatis similique ipsum illo dignissimos.</p>
+                                <span class="d-block mb-2 date">15-03-2023</span>
+                                <a href="FIXME:" class="btn btn-custom px-4 py-2 d-flex align-items-center justify-content-center">Read More <i class="fas fa-arrow-right ml-2"></i></a>
                             </div>
-                        </section>
-                        <section class="investment-lists pt-4 pb-5">
-                          
+                        </div>
+                        <div class="col-4 mb-3">
+                            <div class="custom-card">
+                                <figure>
+                                    <img src="/images/common/mainfarm.png" class="w-100" alt="">
+                                </figure>
+                                <p class="mb-2 title">Agro Farming Project</p>
+                                <p class="mb-2 content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati facere veritatis similique ipsum illo dignissimos.</p>
+                                <span class="d-block mb-2 date">15-03-2023</span>
+                                <a href="FIXME:" class="btn btn-custom px-4 py-2 d-flex align-items-center justify-content-center">Read More <i class="fas fa-arrow-right ml-2"></i></a>
+                            </div>
+                        </div><div class="col-4 mb-3">
+                            <div class="custom-card">
+                                <figure>
+                                    <img src="/images/common/mainfarm.png" class="w-100" alt="">
+                                </figure>
+                                <p class="mb-2 title">Agro Farming Project</p>
+                                <p class="mb-2 content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati facere veritatis similique ipsum illo dignissimos.</p>
+                                <span class="d-block mb-2 date">15-03-2023</span>
+                                <a href="FIXME:" class="btn btn-custom px-4 py-2 d-flex align-items-center justify-content-center">Read More <i class="fas fa-arrow-right ml-2"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-4 mb-3">
+                            <div class="custom-card">
+                                <figure>
+                                    <img src="/images/common/mainfarm.png" class="w-100" alt="">
+                                </figure>
+                                <p class="mb-2 title">Agro Farming Project</p>
+                                <p class="mb-2 content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati facere veritatis similique ipsum illo dignissimos.</p>
+                                <span class="d-block mb-2 date">15-03-2023</span>
+                                <a href="FIXME:" class="btn btn-custom px-4 py-2 d-flex align-items-center justify-content-center">Read More <i class="fas fa-arrow-right ml-2"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-4 mb-3">
+                            <div class="custom-card">
+                                <figure>
+                                    <img src="/images/common/mainfarm.png" class="w-100" alt="">
+                                </figure>
+                                <p class="mb-2 title">Agro Farming Project</p>
+                                <p class="mb-2 content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati facere veritatis similique ipsum illo dignissimos.</p>
+                                <span class="d-block mb-2 date">15-03-2023</span>
+                                <a href="FIXME:" class="btn btn-custom px-4 py-2 d-flex align-items-center justify-content-center">Read More <i class="fas fa-arrow-right ml-2"></i></a>
+                            </div>
+                        </div><div class="col-4 mb-3">
+                            <div class="custom-card">
+                                <figure>
+                                    <img src="/images/common/mainfarm.png" class="w-100" alt="">
+                                </figure>
+                                <p class="mb-2 title">Agro Farming Project</p>
+                                <p class="mb-2 content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati facere veritatis similique ipsum illo dignissimos.</p>
+                                <span class="d-block mb-2 date">15-03-2023</span>
+                                <a href="FIXME:" class="btn btn-custom px-4 py-2 d-flex align-items-center justify-content-center">Read More <i class="fas fa-arrow-right ml-2"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>                          
             <!-- Start Dynamic Sections Ends here -->
         </div>
     </div>
